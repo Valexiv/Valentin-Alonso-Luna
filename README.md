d4f25bac1a7d787a3fa41af45d263a29fc802e10
Valexiv <va133905@gmail.com>
2025-10-20 03:15:28 -0700

    API fetch completed

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Valentin-Alonso-Luna
 portfolio project for Intro to Programming course with Code the Dream 
 Valentin Alonso Evaristo 
+heres the link to my api project [Open API GitHub Repository] https://github.com/Valexiv/Open-API-Project-LUNA
--- js/index.js
+++ js/index.js
@@ -15,3 +15,14 @@ for (let i = 0; i < skills.length; i++) {
   skill.innerText = skills[i];                    
   skillsList.appendChild(skill);                  
 }
+
+fetch('https://api.github.com/users/Valexiv/repos') 
+.then(response => response.json())
+ .then(data=>{ console.log("Fetched repos: ", data); 
+  return data;
+ }) 
+ .then(data=>{ let repositories=data; console.log ("repositories: ", repositories);
+   let projectSection= document.getElementById("Projects"); let projectList=projectSection.querySelector("ul"); 
+   for (let i=0;i< repositories.length; i++)
+     { let project=document.createElement("li"); project.innerText= repositories[i].name; projectList.appendChild(project); }; })
+   .catch(error=>{ console.error("Error fetching repositories: ", error); });
